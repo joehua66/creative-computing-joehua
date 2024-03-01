0341f3ac73d170bea3dd333f270c67014e895e60
joehua66 <johndoe@example.com>
2024-02-29 21:26:25 -0500

    particles

--- code/week-05/00-citibike/data/sketch.js
+++ code/week-05/00-citibike/data/sketch.js
@@ -4,11 +4,17 @@ let stations=[]
 let currentTime;
 
 function preload(){
-    data = loadJSON('./2023-11-06.json');
+    data = loadJSON('./data.json');
 }
 
 async function setup(){
 createCanvas(600,600)
+
+data.stations.forEach( s => {
+    const station= new Station(s);
+    stations.push(station);
+})
+
 const lats = data.stations.map( station => station.lat)
 maxLat = Math.max (... lats)
 maxLng = Math.max(...data.stations.map(station => station.lng))
@@ -17,10 +23,7 @@ minLng = Math.min(...data.stations.map(station => station.lng))
 
 console.log(maxLat,maxLng,minLat,minLng)
 
-data.stations.forEach( s => {
-    const station= new Station(s);
-    stations.push(station);
-})
+
 
 currentTime = data.trips(0).st;
 maxTime = data.trips (data.trips.length-1).et
---
+++ code/week-07/rigidBody.js
@@ -0,0 +1,37 @@
+class RigidBody{
+    constructor(x,y,size){
+        this.loc=createVector(x,y)
+        this.vel = createVector();
+        this.acc = createVector();
+        this.size=size
+        this.friction=0.985
+
+    }
+
+    applyForce(force){
+        this.acc.add(force);
+    }
+
+    update(){
+        this.vel.add(this.acc)
+        this.loc.add(this.vel)
+        this.acc.mult(0)
+        this.vel.mult(this.friction)
+    }
+
+    bounce(){
+        let top = this.size/2
+        let bottom = height-this.size/2;
+        let left = this.size/2
+        let right = width-this.size/2
+        if(this.loc.y<top || this.loc.y > bottom){
+            this.vel.y   *= -1
+        }
+    }
+
+    display(){
+        circle(this.loc.x,this.loc.y,this.size)
+
+
+    }
+}
